9645e9f219866ff9d7a62ededf7569591f989aa2
anthony <contact@achao.io>
2025-04-09 23:35:16 -0700

    add stopwatch route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,18 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
   return (
-    <div>
+    <div className="flex gap-4 justify-center">
       <Link
-        className="hover:bg-gray-800 border border-gray-500 bg-black p-4 rounded-md"
+        className="hover:bg-gray-800 border border-gray-500 bg-black p-4 rounded-md w-48 text-center"
         to="/rock-paper-scissors"
       >
         Rock Paper Scissors
+      </Link>
+      <Link
+        className="hover:bg-gray-800 border border-gray-500 bg-black p-4 rounded-md w-48 text-center"
+        to="/stopwatch"
+      >
+        Stopwatch
       </Link>
     </div>
   )
